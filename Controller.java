775cdb6cae7eefc3cb714f7e03d0892f0cb9dc67
Aarno T <Aarno T>
2017-04-21 18:50:31 +0300

    Move some code to the controller + add the main loop

---
+++ Controller.java
@@ -0,0 +1,50 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+
+public class Controller extends Application {
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+ 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Drawing Operations Test");
+        Group root = new Group();
+        Canvas canvas = new Canvas(800, 600);
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        // Need reference to the controller for the animation timer.
+        Controller controller = this;
+
+        new AnimationTimer(){
+            private long previousNanoTime;
+            public void handle(long currentNanoTime){
+                if (previousNanoTime == 0) {
+                    this.previousNanoTime = currentNanoTime;
+                }
+                // Time since previous loop in milliseconds.
+                double deltaTime = (currentNanoTime - this.previousNanoTime) / 1000000.0;
+
+                controller.updateModel(deltaTime);
+                controller.updateView(gc);
+            }
+        }.start();
+
+        primaryStage.show();
+    }
+
+    void updateModel(double deltaTime) {
+    }
+
+    void updateView(GraphicsContext gc) {
+        View.drawAmmo(gc);
+    }
+}
--- View.java
+++ View.java
@@ -1,29 +1,9 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
-public class View extends Application {
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
- 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(800, 600);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
+public class View {
+    static public void drawAmmo(GraphicsContext gc) {
         gc.setFill(Color.RED);
         gc.fillOval(375, 275, 50, 50);
-
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
     }
 }

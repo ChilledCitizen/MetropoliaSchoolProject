888f1b8aca1069754d5d62d7026070d9c971c6b2
Aarno T <Aarno T>
2017-04-24 11:44:42 +0300

    Add drawEnemy method

--- Packages/view/View.java
+++ Packages/view/View.java
@@ -3,6 +3,8 @@ package view;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+import enemy.Enemy;
+
 public class View {
 
     static public void drawBackground(GraphicsContext gc, double width, double height) {
@@ -18,5 +20,9 @@ public class View {
     static public void drawWall(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
         gc.fillRect(250, 350, 25, 250); //(x, y, w, h)
+    }
+
+    static public void drawEnemy(GraphicsContext gc, Enemy enemy) {
+        gc.drawImage(enemy.getImage(), enemy.getPosition(), 500);
     }
 }

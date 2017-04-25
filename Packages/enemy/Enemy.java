db7df1df6d88e580459f56bb4a8308a0de927af6
Aarno T <Aarno T>
2017-04-25 10:21:48 +0300

    Move some variables out of the constructor

--- Packages/enemy/Enemy.java
+++ Packages/enemy/Enemy.java
@@ -8,21 +8,17 @@ import javafx.scene.image.Image;
 
 public class Enemy {
     private double position;
-    private int speed;
-    private int HP;
-    private int frameDuration;
-    public boolean visible;
-    private double milliSeconds;
-    private HashMap<String, ArrayList<Image>> states;
-    private String state;
+    private int speed = 1;
+    private int frameDuration = 100;
+    public boolean visible = true;
+    private double milliSeconds = 0;
+    private HashMap<String, ArrayList<Image>> states = new HashMap(4);
+    private String state = "walking";
     private Iterator<Image> animation;
     private Image image;
 
     public Enemy(double position) {
         this.position = position;
-        this.HP = 100;
-        this.speed = 1;
-        this.visible = true;
         ArrayList idle = new ArrayList<Image>(15);
         ArrayList walking = new ArrayList<Image>(10);
         ArrayList dead = new ArrayList<Image>(12);
@@ -40,15 +36,11 @@ public class Enemy {
             fileName = String.format("images/zombie_animation/%s/Dead (%d).png", gender, frame);
             dead.add(new Image(fileName, 120, 110, true, true));
         }
-        this.states = new HashMap<String, ArrayList<Image>>(4);
         this.states.put("idle", idle);
         this.states.put("walking", walking);
         this.states.put("dead", dead);
-        this.state = "walking";
         this.animation = walking.iterator();
         this.image = this.animation.next();
-        this.milliSeconds = 0;
-        this.frameDuration = 100;
     }
 
     public Image getImage() {

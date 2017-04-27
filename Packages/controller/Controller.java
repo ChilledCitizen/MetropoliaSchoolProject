8af51d5c14b379965a69166d9da425cdbc41fb83
Aarno T <Aarno T>
2017-04-27 12:29:30 +0300

    Add keyboard input

--- Packages/controller/Controller.java
+++ Packages/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.util.ArrayList;
+import java.awt.geom.Point2D;
 import java.awt.geom.Ellipse2D;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -10,6 +11,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.event.EventHandler;
 
 import view.View;
 import enemy.Enemy;
@@ -25,14 +29,6 @@ public class Controller extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(800, 600);
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        // Need reference to the controller for the animation timer.
-        Controller controller = this;
         ArrayList<Enemy> enemyList = new ArrayList<Enemy>(10);
         for (int i = 1; i < 4; i++) {
             enemyList.add(new Enemy(400 + i*100));
@@ -40,6 +36,36 @@ public class Controller extends Application {
         Wall wall = new Wall(100,10,50,100);
         Catapult cp = new Catapult(0);
         Ammo ammo = new Ammo();
+
+        primaryStage.setTitle("Drawing Operations Test");
+        Group root = new Group();
+        Canvas canvas = new Canvas(800, 600);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent event)
+                {
+                    switch(event.getCode()) {
+                        case SPACE:
+                            if (ammo.position.equals(new Point2D.Double(100.0, 400.0))
+                                    && ammo.velocity.equals(new Point2D.Double(0.0, 0.0))) {
+                                ammo.enableGravity();
+                                ammo.velocity.setLocation(10.0, -15.0);
+                                }
+                            break;
+                        case R:
+                            ammo.position.setLocation(100.0, 400.0);
+                            ammo.stop();
+                            break;
+                    }
+                }
+            });
+        primaryStage.setScene(scene);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        // Need reference to the controller for the animation timer.
+        Controller controller = this;
 
         new AnimationTimer() {
             private long previousNanoTime;
